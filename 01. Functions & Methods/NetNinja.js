247a372f916cf0c3d42481666718d9f7eac7c0ff
okpreetam <preetamcnc@gmail.com>
2021-05-22 21:43:08 +0530

    Adding my files

---
+++ 01. Functions & Methods/NetNinja.js
@@ -0,0 +1,15 @@
+const greet = function (moto) {
+    console.log('hello ', moto);
+}
+
+greet("kuku");
+
+greet("alan");
+
+greet("isas");
+
+const hello = "yahoo";
+
+console.log(hello.length);
+
+
---
+++ 02. Object Literals/NetNinja.js
@@ -0,0 +1,13 @@
+const greet = function (moto) {
+  console.log("hello ", moto);
+};
+
+greet("kuku");
+
+greet("alan");
+
+greet("isas");
+
+greet("jojo");
+
+const yolo = "yahoo"
